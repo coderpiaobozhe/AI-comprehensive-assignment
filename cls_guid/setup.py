25af26444be29af6af7b7e08d32bcab76c5e10ed
JC <63344459+NUAAerJC@users.noreply.github.com>
2023-01-03 12:59:01 +0800

    Update setup.py

--- cls_guid/setup.py
+++ cls_guid/setup.py
@@ -1,1 +1,7 @@
+from setuptools import setup
 
+setup(
+    name="classifier_guided-diffusion",
+    py_modules=["classifier_guided_diffusion"],
+    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+)
